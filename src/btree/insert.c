78f32db6ea5f2cd5593f945d9bd03326d81b8258
Alexander Korotkov <akorotkov@postgresql.org>
2022-05-07 02:05:24 +0300

    Fix2

--- src/btree/insert.c
+++ src/btree/insert.c
@@ -586,7 +586,9 @@ o_btree_insert_split(BTreeInsertStackItem *insert_item,
 					 OffsetNumber offset,
 					 CommitSeqNo csn,
 					 bool needsUndo,
-					 int reserve_kind)
+					 int reserve_kind,
+					 int *waitersWakeupProcnums,
+					 int waitersWakeupCount)
 {
 	OffsetNumber left_count;
 	OBTreeFindPageContext *curContext = insert_item->context;
@@ -633,6 +635,11 @@ o_btree_insert_split(BTreeInsertStackItem *insert_item,
 					   csn, undoLocation);
 
 	unlock_page(right_blkno);
+
+	if (waitersWakeupCount > 0)
+		wakeup_waiters_with_tuples(blkno,
+									waitersWakeupProcnums,
+									waitersWakeupCount);
 
 	o_btree_insert_mark_split_finished_if_needed(insert_item);
 	insert_item->left_blkno = blkno;
@@ -899,10 +906,8 @@ o_btree_insert_item(BTreeInsertStackItem *insert_item, int reserve_kind)
 											lockerState);
 				}
 			}
-			if (waitersWakeupCount > 0)
-				wakeup_waiters_with_tuples(blkno,
-										   tupleWaiterProcnums,
-										   waitersWakeupCount);
+
+			Assert(items.itemsCount + waitersWakeupCount == newItems.itemsCount);
 
 			if (!split)
 			{
@@ -910,6 +915,12 @@ o_btree_insert_item(BTreeInsertStackItem *insert_item, int reserve_kind)
 				perform_page_compaction(desc, blkno, &newItems, needsUndo, csn);
 				o_btree_insert_mark_split_finished_if_needed(insert_item);
 				MARK_DIRTY(desc->ppool, blkno);
+
+				if (waitersWakeupCount > 0)
+					wakeup_waiters_with_tuples(blkno,
+											   tupleWaiterProcnums,
+											   waitersWakeupCount);
+
 				unlock_page(blkno);
 				END_CRIT_SECTION();
 				next = true;
@@ -917,7 +928,9 @@ o_btree_insert_item(BTreeInsertStackItem *insert_item, int reserve_kind)
 			else
 			{
 				next = o_btree_insert_split(insert_item, &newItems, offset, csn,
-											needsUndo, reserve_kind);
+											needsUndo, reserve_kind,
+											tupleWaiterProcnums,
+											waitersWakeupCount);
 			}
 		}
 		else
@@ -1094,7 +1107,7 @@ o_btree_insert_item(BTreeInsertStackItem *insert_item, int reserve_kind)
 							 csn);
 
 			next = o_btree_insert_split(insert_item, &items, offset, csn,
-										needsUndo, reserve_kind);
+										needsUndo, reserve_kind, NULL, 0);
 		}
 
 		if (next)
