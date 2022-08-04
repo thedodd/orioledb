cb5f7db8109525b4b3e3169330c006be952bd3b6
Tanyaevm <evm.tanya.qa@gmail.com>
2022-08-04 15:39:47 +0300

    test checkpoint rll

--- t/checkpoint_update_test.py
+++ t/checkpoint_update_test.py
@@ -92,3 +92,382 @@ class CheckpointUpdateTest(CheckpointUpdateBaseTest):
 		node.safe_psql("SELECT * FROM o_checkpoint;")
 		self.assertEqual(node.execute("SELECT COUNT(*) FROM o_checkpoint;")[0][0], 3)
 		node.stop()
+
+	def test_checkpoint_rll3(self):
+		node = self.node
+		node.start()
+		node.safe_psql('postgres', """
+			CREATE EXTENSION IF NOT EXISTS orioledb;
+			CREATE TABLE IF NOT EXISTS o_checkpoint (
+				id int NOT NULL,
+				val_1 int,
+				val_2 int,
+				PRIMARY KEY (id)
+			) USING orioledb;
+			INSERT INTO o_checkpoint
+				(SELECT id, id, id FROM generate_series(1, 3) id);
+		""")
+
+		node.safe_psql("""
+		  CREATE TYPE o_enum AS ENUM ('a', 'b');
+		  CREATE TABLE o_test_typecaches (key o_enum, PRIMARY KEY(key)) USING orioledb;
+		""")
+
+		con1 = node.connect()
+		con2 = node.connect()
+
+		con1.begin()
+		con2.begin()
+		con1.execute("SELECT * FROM o_checkpoint WHERE id BETWEEN 1 and 2 FOR KEY SHARE;")
+		con2.execute("SELECT * FROM o_checkpoint WHERE id BETWEEN 2 and 3 FOR NO KEY UPDATE;")
+
+		node.safe_psql("CHECKPOINT;")
+
+		con1.commit()
+		con2.commit()
+
+		con1.close()
+		con2.close()
+
+		node.stop(['-m', 'immediate'])
+		node.start()
+
+		node.safe_psql("SELECT * FROM o_checkpoint;")
+		self.assertEqual(node.execute("SELECT COUNT(*) FROM o_checkpoint;")[0][0], 3)
+		node.stop()
+
+	def test_checkpoint_rll4(self):
+		node = self.node
+		node.start()
+		node.safe_psql('postgres', """
+			CREATE EXTENSION IF NOT EXISTS orioledb;
+			CREATE TABLE IF NOT EXISTS o_checkpoint (
+				id int NOT NULL,
+				val_1 int,
+				val_2 int,
+				PRIMARY KEY (id)
+			) USING orioledb;
+			INSERT INTO o_checkpoint
+				(SELECT id, id, id FROM generate_series(1, 3) id);
+		""")
+
+		node.safe_psql("""
+     		UPDATE o_checkpoint SET val_2 = 10 WHERE val_1 = 2;
+    	""")
+
+		con1 = node.connect()
+		con2 = node.connect()
+
+		con1.begin()
+		con2.begin()
+		con1.execute("SELECT * FROM o_checkpoint WHERE id BETWEEN 1 and 2 FOR KEY SHARE;")
+		con2.execute("SELECT * FROM o_checkpoint WHERE id BETWEEN 2 and 3 FOR NO KEY UPDATE;")
+
+		node.safe_psql("CHECKPOINT;")
+
+		con1.commit()
+		con2.commit()
+
+		con1.close()
+		con2.close()
+
+		node.stop(['-m', 'immediate'])
+		node.start()
+
+		node.safe_psql("SELECT * FROM o_checkpoint;")
+		self.assertEqual(node.execute("SELECT COUNT(*) FROM o_checkpoint;")[0][0], 3)
+		node.stop()
+
+	def test_checkpoint_rll5(self):
+		node = self.node
+		node.start()
+		node.safe_psql('postgres', """
+			CREATE EXTENSION IF NOT EXISTS orioledb;
+			CREATE TABLE IF NOT EXISTS o_checkpoint (
+				id int NOT NULL,
+				val_1 int,
+				val_2 int,
+				val_3 int,
+				PRIMARY KEY (id)
+			) USING orioledb;
+			INSERT INTO o_checkpoint
+				(SELECT id, id, id, id FROM generate_series(1, 3) id);
+		""")
+
+		node.safe_psql("""
+     		DELETE FROM o_checkpoint WHERE val_3 = 1;
+    	""")
+
+		con1 = node.connect()
+		con2 = node.connect()
+
+		con1.begin()
+		con2.begin()
+		con1.execute("SELECT * FROM o_checkpoint WHERE id BETWEEN 1 and 2 FOR KEY SHARE;")
+		con2.execute("SELECT * FROM o_checkpoint WHERE id BETWEEN 2 and 3 FOR NO KEY UPDATE;")
+
+		node.safe_psql("CHECKPOINT;")
+
+		con1.commit()
+		con2.commit()
+
+		con1.close()
+		con2.close()
+
+		node.stop(['-m', 'immediate'])
+		node.start()
+
+		node.safe_psql("SELECT * FROM o_checkpoint;")
+		self.assertEqual(node.execute("SELECT COUNT(*) FROM o_checkpoint;")[0][0], 2)
+		node.stop()
+
+	def test_checkpoint_rll6(self):
+		node = self.node
+		node.start()
+		node.safe_psql('postgres', """
+			CREATE EXTENSION IF NOT EXISTS orioledb;
+			CREATE TABLE IF NOT EXISTS o_checkpoint (
+				id int NOT NULL,
+				val_1 int,
+				val_2 int,
+				val_3 int,
+				PRIMARY KEY (id)
+			) USING orioledb;
+			INSERT INTO o_checkpoint
+				(SELECT id, id, id, id FROM generate_series(1, 3) id);
+		""")
+
+		node.safe_psql("""
+     		TRUNCATE o_checkpoint;
+			INSERT INTO o_checkpoint
+				(SELECT id, id, id, id FROM generate_series(1, 3) id);
+    	""")
+
+		con1 = node.connect()
+		con2 = node.connect()
+
+		con1.begin()
+		con2.begin()
+		con1.execute("SELECT * FROM o_checkpoint WHERE id BETWEEN 1 and 2 FOR KEY SHARE;")
+		con2.execute("SELECT * FROM o_checkpoint WHERE id BETWEEN 2 and 3 FOR NO KEY UPDATE;")
+
+		node.safe_psql("CHECKPOINT;")
+
+		con1.commit()
+		con2.commit()
+
+		con1.close()
+		con2.close()
+
+		node.stop(['-m', 'immediate'])
+		node.start()
+
+		node.safe_psql("SELECT * FROM o_checkpoint;")
+		self.assertEqual(node.execute("SELECT COUNT(*) FROM o_checkpoint;")[0][0], 3)
+		node.stop()
+
+	def test_checkpoint_rll7(self):
+		node = self.node
+		node.start()
+		node.safe_psql('postgres', """
+			CREATE EXTENSION IF NOT EXISTS orioledb;
+			CREATE TABLE IF NOT EXISTS o_checkpoint (
+				id int NOT NULL,
+				val_1 int,
+				val_2 int,
+				val_3 int,
+				PRIMARY KEY (id)
+			) USING orioledb;
+			INSERT INTO o_checkpoint
+				(SELECT id, id, id, id FROM generate_series(1, 3) id);
+		""")
+
+		node.safe_psql("""
+     		ALTER TABLE o_checkpoint DROP COLUMN val_3;
+    	""")
+
+		con1 = node.connect()
+		con2 = node.connect()
+
+		con1.begin()
+		con2.begin()
+		con1.execute("SELECT * FROM o_checkpoint WHERE id BETWEEN 1 and 2 FOR KEY SHARE;")
+		con2.execute("SELECT * FROM o_checkpoint WHERE id BETWEEN 2 and 3 FOR NO KEY UPDATE;")
+
+		node.safe_psql("CHECKPOINT;")
+
+		con1.commit()
+		con2.commit()
+
+		con1.close()
+		con2.close()
+
+		node.stop(['-m', 'immediate'])
+		node.start()
+
+		node.safe_psql("SELECT * FROM o_checkpoint;")
+		self.assertEqual(node.execute("SELECT COUNT(*) FROM o_checkpoint;")[0][0], 3)
+		node.stop()
+
+	def test_checkpoint_rll8(self):
+		node = self.node
+		node.start()
+		node.safe_psql('postgres', """
+			CREATE EXTENSION IF NOT EXISTS orioledb;
+			CREATE TABLE IF NOT EXISTS o_checkpoint (
+				id int NOT NULL,
+				val_1 int,
+				val_2 int,
+				PRIMARY KEY (id)
+			) USING orioledb;
+			INSERT INTO o_checkpoint
+				(SELECT id, id, id FROM generate_series(1, 3) id);
+		""")
+
+		node.safe_psql("""
+     		ALTER TABLE o_checkpoint ADD COLUMN val_3 int;
+    	""")
+
+		con1 = node.connect()
+		con2 = node.connect()
+
+		con1.begin()
+		con2.begin()
+		con1.execute("SELECT * FROM o_checkpoint WHERE id BETWEEN 1 and 2 FOR KEY SHARE;")
+		con2.execute("SELECT * FROM o_checkpoint WHERE id BETWEEN 2 and 3 FOR NO KEY UPDATE;")
+
+		node.safe_psql("CHECKPOINT;")
+
+		con1.commit()
+		con2.commit()
+
+		con1.close()
+		con2.close()
+
+		node.stop(['-m', 'immediate'])
+		node.start()
+
+		node.safe_psql("SELECT * FROM o_checkpoint;")
+		self.assertEqual(node.execute("SELECT COUNT(*) FROM o_checkpoint;")[0][0], 3)
+		node.stop()
+
+	def test_checkpoint_rll9(self):
+		node = self.node
+		node.start()
+		node.safe_psql('postgres', """
+			CREATE EXTENSION IF NOT EXISTS orioledb;
+			CREATE TABLE IF NOT EXISTS o_checkpoint (
+				id int NOT NULL,
+				val_1 int,
+				val_2 int,
+				PRIMARY KEY (id)
+			) USING orioledb;
+			INSERT INTO o_checkpoint
+				(SELECT id, id, id FROM generate_series(1, 3) id);
+		""")
+
+		node.safe_psql("""
+     		ALTER TABLE o_checkpoint RENAME COLUMN val_2 TO val_3;
+    	""")
+
+		con1 = node.connect()
+		con2 = node.connect()
+
+		con1.begin()
+		con2.begin()
+		con1.execute("SELECT * FROM o_checkpoint WHERE id BETWEEN 1 and 2 FOR KEY SHARE;")
+		con2.execute("SELECT * FROM o_checkpoint WHERE id BETWEEN 2 and 3 FOR NO KEY UPDATE;")
+
+		node.safe_psql("CHECKPOINT;")
+
+		con1.commit()
+		con2.commit()
+
+		con1.close()
+		con2.close()
+
+		node.stop(['-m', 'immediate'])
+		node.start()
+
+		node.safe_psql("SELECT * FROM o_checkpoint;")
+		self.assertEqual(node.execute("SELECT COUNT(*) FROM o_checkpoint;")[0][0], 3)
+		node.stop()
+
+	def test_checkpoint_rll10(self):
+		node = self.node
+		node.start()
+		node.safe_psql('postgres', """
+			CREATE EXTENSION IF NOT EXISTS orioledb;
+			CREATE TABLE IF NOT EXISTS o_checkpoint (
+				id int NOT NULL,
+				PRIMARY KEY (id)
+			)USING orioledb;
+		""")
+
+		node.safe_psql("""
+     		INSERT INTO o_checkpoint
+				(SELECT id FROM generate_series(1, 3) id);
+    	""")
+
+		con1 = node.connect()
+		con2 = node.connect()
+
+		con1.begin()
+		con2.begin()
+		con1.execute("SELECT * FROM o_checkpoint WHERE id BETWEEN 1 and 2 FOR KEY SHARE;")
+		con2.execute("SELECT * FROM o_checkpoint WHERE id BETWEEN 2 and 3 FOR NO KEY UPDATE;")
+
+		node.safe_psql("CHECKPOINT;")
+
+		con1.commit()
+		con2.commit()
+
+		con1.close()
+		con2.close()
+
+		node.stop(['-m', 'immediate'])
+		node.start()
+
+		node.safe_psql("SELECT * FROM o_checkpoint;")
+		self.assertEqual(node.execute("SELECT COUNT(*) FROM o_checkpoint;")[0][0], 3)
+		node.stop()
+
+	def test_checkpoint_rll11(self):
+		node = self.node
+		node.start()
+		node.safe_psql('postgres', """
+			CREATE EXTENSION IF NOT EXISTS orioledb;
+			CREATE TABLE IF NOT EXISTS o_checkpoint (
+				id int NOT NULL
+			)USING orioledb;
+
+			INSERT INTO o_checkpoint
+				(SELECT id FROM generate_series(1, 3) id);
+		""")
+
+		node.safe_psql("""
+     		ALTER TABLE o_checkpoint ADD PRIMARY KEY(id);
+    	""")
+
+		con1 = node.connect()
+		con2 = node.connect()
+
+		con1.begin()
+		con2.begin()
+		con1.execute("SELECT * FROM o_checkpoint WHERE id BETWEEN 1 and 2 FOR KEY SHARE;")
+		con2.execute("SELECT * FROM o_checkpoint WHERE id BETWEEN 2 and 3 FOR NO KEY UPDATE;")
+
+		node.safe_psql("CHECKPOINT;")
+
+		con1.commit()
+		con2.commit()
+
+		con1.close()
+		con2.close()
+
+		node.stop(['-m', 'immediate'])
+		node.start()
+
+		node.safe_psql("SELECT * FROM o_checkpoint;")
+		self.assertEqual(node.execute("SELECT COUNT(*) FROM o_checkpoint;")[0][0], 3)
+		node.stop()
